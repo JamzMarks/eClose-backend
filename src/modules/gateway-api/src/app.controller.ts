efc15f9079f0addc781b710801238781a65f97aa
JamzMarks <jamzmarks@gmail.com>
2025-05-30 17:11:44 -0300

    FEAT: added libs common

--- src/modules/gateway-api/src/app.controller.ts
+++ src/modules/gateway-api/src/app.controller.ts
@@ -1,17 +1,17 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Inject, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+  ){}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
-  @Post()
-  postHello(): string {
-    return this.appService.postHello();
-  }
+  
 }
--- src/modules/gateway-api/src/controllers/user.controller.ts
+++ src/modules/gateway-api/src/controllers/user.controller.ts
@@ -1,20 +1,12 @@
-import { Controller, Get } from "@nestjs/common";
-import { ClientProxy, ClientProxyFactory, Transport } from "@nestjs/microservices";
+import { Controller, Get, Inject } from "@nestjs/common";
+import { ClientProxy } from "@nestjs/microservices";
 
 @Controller('user')
 export class UserController {
-    private client: ClientProxy;
-    constructor() {
-    
-        
-    this.client = ClientProxyFactory.create({
-      transport: Transport.TCP,
-      options: { host: 'localhost', port: 3001 },
-    });
-  }
+    constructor(
+        @Inject('USER_SERVICE') private readonly userClient: ClientProxy) {}
     @Get()
     async getUsers() {
-        const users = await this.client.send({ cmd: 'get_users' }, {}).toPromise();
-        return users;
+        return this.userClient.send({ cmd: 'get_users' }, {}).toPromise();
     }
 }
